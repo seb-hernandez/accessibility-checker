39b6b32fd49f690a6ed50d8a44d1e2d569ead0ae
Sebastian Hernandez <sebashr0920@gmail.com>
2024-06-13 14:01:41 +0200

    initial commit

--- acc/main.m
+++ acc/main.m
@@ -1,16 +1,52 @@
+//#import <Foundation/Foundation.h>
+//#import <AppKit/AppKit.h>
 //
-//  main.m
-//  acc
+//BOOL checkAccessibilityPermissions(void) {
+////    NSDictionary *options = @{(__bridge id)kAXTrustedCheckOptionPrompt: @NO};
+////    return AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options);
+//    return AXIsProcessTrusted();
+//}
 //
-//  Created by Sebastian Hernandez on 2024-06-13.
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        // Replace with your application's bundle identifier
+//        NSString *bundleIdentifier = @"com.test.acc";
 //
+//        BOOL accessibilityEnabled = NO;
+//
+//        // Loop to check accessibility permissions every 5 seconds
+//        while (!accessibilityEnabled) {
+//            accessibilityEnabled = checkAccessibilityPermissions();
+//
+//            if (accessibilityEnabled) {
+//                NSLog(@"Accessibility permissions granted for %@", bundleIdentifier);
+//            } else {
+//                NSLog(@"Waiting for accessibility permissions for %@", bundleIdentifier);
+//                sleep(1); // Wait for 5 seconds before checking again
+//            }
+//        }
+//    }
+//    return 0;
+//}
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Replace with your application's bundle identifier
+        NSString *bundleIdentifier = @"com.test.acc";
+
+        // Request accessibility permissions
+        NSDictionary *options = @{(__bridge id)kAXTrustedCheckOptionPrompt: @YES};
+        BOOL accessibilityEnabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options);
+
+        if (accessibilityEnabled) {
+            NSLog(@"Accessibility permissions granted for %@", bundleIdentifier);
+        } else {
+            NSLog(@"Failed to grant accessibility permissions for %@", bundleIdentifier);
+        }
     }
     return 0;
 }
+
